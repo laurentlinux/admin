e404ebd1b4c5a0e1a68bfc78df9a7443cec29a1a
Alexis Gavoty <kload@kload.fr>
2012-06-06 10:09:40 +0200

    All the people are not dummies

--- controllers/domain.php
+++ controllers/domain.php
@@ -1,7 +1,7 @@
 <?php
 
  /**
-  *  Yunohost - Self-hosting for dummies
+  *  Yunohost - Self-hosting for all
   *  Copyright (C) 2012  Kload <kload@kload.fr>
   *
   *  This program is free software: you can redistribute it and/or modify
